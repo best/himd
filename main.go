63ae0903b5a88bc96fc02c57fbbc369fde6833ca
best <XThundering@vip.qq.com>
2019-08-13 15:07:07 +0800

    remove verify file, move flagVerify to main func

--- main.go
+++ main.go
@@ -3,7 +3,9 @@ package main
 import (
 	"flag"
 	"fmt"
+	"log"
 	"os"
+	"regexp"
 	"runtime"
 )
 
@@ -48,5 +50,27 @@ func main() {
 	}
 
 	// 检查输入参数是否符合要求
-	verifyFlag(url, thread)
+	verifyFlag()
+}
+
+// verifyFlag 检查flag是否符合要求
+func verifyFlag() {
+	// 如果下载链接未指定，输出错误信息并退出程序
+	if url == "" {
+		log.Fatalln("error: no specifies download target, `-u` option can not be null")
+	}
+
+	// 校验url是否符合要求
+	if ret, err := regexp.MatchString("^((https|http|ftp)?://)[^\\s]+", url); !ret {
+		if err != nil {
+			log.Fatalln("flag verify failed, err: ", err.Error())
+		} else {
+			log.Fatalln("url must format as [https/http/ftp]://target.url")
+		}
+	}
+
+	// 校验thread是否符合要求
+	if thread <= 0 {
+		log.Fatalln("thread num must max than 0")
+	}
 }
